5114ac38a4a14bb5cfc75e3619e44d329cafc0af
Axel Plowman <axel.plowman@gmail.com>
2015-12-09 16:24:26 +0100

    update

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ analyser.FFTSize = 2048; //sets analyser's FFTSize-property
 //biquadFilter settings
 biquadFilter.type = "lowpass";
 biquadFilter.frequency.value = 1000;
-biquadFilter.gain.value = 25;
+biquadFilter.Q.value = 25;
+console.log("filtertype="biquadFilter.type + ", frequency=" + biquadFilter.frequency.value + ", Q-value=" + biquadFilter.Q.value);
 
 
 //Audio specificeren:
