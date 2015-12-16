012db9ca3542513393cc1a343e66709d2989524d
Axel Plowman <axel.plowman@gmail.com>
2015-12-16 13:49:51 +0100

    commit

--- script.js
+++ script.js
@@ -93,9 +93,9 @@ for (var i = 0; i < equaliserNodes.length; i++) {
 //     }; 
 
 
-document.querySelector("#listSongs").addEventListener('click', function () {
-	alert("succes!");
-});
+// document.querySelector("#listSongs").addEventListener('click', function () {
+// 	alert("succes!");
+// });
 
 
 //range slider inputs to equaliser nodes
