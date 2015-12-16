e4f1bdebdafb26db551c64583f9a8a17e9e850c0
Axel Plowman <axel.plowman@gmail.com>
2015-12-16 23:24:54 +0100

    test2

--- script.js
+++ script.js
@@ -165,13 +165,13 @@ source1.connect(splitterNodes[0]);
 source2.connect(splitterNodes[1]);
 
 //connect LEFT  channel of deck 1 to gain[0]
-splitterNodes[0].connect(gainNodes[0], 0);
+splitterNodes[0].connect(gainNodes[0]);
 //connect RIGHT channel of deck 1 to gain[1]
-splitterNodes[0].connect(gainNodes[1], 1);
+//splitterNodes[0].connect(gainNodes[1], 1);
 //connect LEFT  channel of deck 2 to gain[2]
-splitterNodes[1].connect(gainNodes[2], 0);
+splitterNodes[1].connect(gainNodes[2]);
 //connect RIGHT channel of deck 2 to gain[3]
-splitterNodes[1].connect(gainNodes[3], 1);
+//splitterNodes[1].connect(gainNodes[3], 1);
 
 gainNodes[0].connect(merger);
 gainNodes[1].connect(merger);
