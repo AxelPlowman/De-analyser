83571990621689c6d90c616b9f0840c06c2a64c6
Axel Plowman <axel.plowman@gmail.com>
2015-12-17 00:07:36 +0100

    test mono

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ source2.connect(splitterNodes[1]);
 //connect LEFT  channel of deck 1 to gain[0]
 splitterNodes[0].connect(gainNodes[0], 0, 0);
 //connect RIGHT channel of deck 1 to gain[1]
-splitterNodes[0].connect(gainNodes[1], 1, 0);
+//splitterNodes[0].connect(gainNodes[1], 1, 0);
 //connect LEFT  channel of deck 2 to gain[2]
 splitterNodes[1].connect(gainNodes[2], 0, 0);
 //connect RIGHT channel of deck 2 to gain[3]
